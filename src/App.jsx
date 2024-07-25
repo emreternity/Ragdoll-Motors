17737b574f4d7f7b0ac9adbc57160d33a2c7a13d
emreternity <akiramotogami13@gmail.com>
2024-07-25 17:25:23 +0300

    page initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./Header.jsx";
+import Page from "./Page.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <body>
       <Header />
+      <Page />
     </body>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/App.css", "./src/App.jsx", "./src/Header.jsx"],
+  content: [
+    "./src/App.css",
+    "./src/App.jsx",
+    "./src/Header.jsx",
+    "./src/Page.jsx",
+  ],
   theme: {
     extend: {},
   },
