45ed81c7d8bb2f316ee24a2f07f652eee5ec3c49
emreternity <akiramotogami13@gmail.com>
2024-07-25 15:54:24 +0300

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header.jsx";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/App.css", "./src/App.jsx", "./src/Header.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
