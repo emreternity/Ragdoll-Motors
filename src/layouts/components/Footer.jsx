f54ff860773d07aa0930e58538f6d71a8edea0e9
emreternity <akiramotogami13@gmail.com>
2024-07-31 19:55:28 +0300

    Footer remake, FAQ page implementation

--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -1,39 +1,29 @@
-import {
-  Footer,
-  FooterBrand,
-  FooterCopyright,
-  FooterDivider,
-  FooterLink,
-  FooterLinkGroup,
-} from "flowbite-react";
-
 import { Link } from "react-router-dom";
+
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 
 export default function FooterFunct() {
   return (
-    <Footer container className="bghead static bottom-0">
-      <div className=" w-full text-center ">
-        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between ">
-          <FooterBrand
-            className="text-white"
-            href="#"
-            src="../images/logo.svg"
-            alt="Ragdoll Logo"
-          />
-          <FooterLinkGroup className="!text-white">
-            <FooterLink href="#">About Us</FooterLink>
-            <FooterLink href="#">Privacy Policy</FooterLink>
-            <FooterLink href="#">Licensing</FooterLink>
-            <FooterLink href="#">Contact</FooterLink>
-          </FooterLinkGroup>
+    <footer className="bghead static bottom-0">
+      <div className=" w-full h-[6.1rem] flex items-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          <div className="flex items-center">
+            <img
+              className="w-8 ml-5"
+              src="../../../images/logo.svg"
+              alt="logo"
+            />
+            <p className="text-white text-xl ml-3 font-light">Ragdoll Motors</p>
+          </div>
+          <div className="grid grid-cols-2 items-center">
+            <Link className="text-white flex items-center" to="/faq">
+              <QuestionMarkCircleIcon className="text-white size-5 mr-1" />
+              F.A.Q.
+            </Link>
+            <p className="text-white mr-5 ml-3">2024</p>
+          </div>
         </div>
-        <FooterCopyright
-          className="text-white"
-          href="#"
-          by="Ragdoll Motors"
-          year={2024}
-        />
       </div>
-    </Footer>
+    </footer>
   );
 }
